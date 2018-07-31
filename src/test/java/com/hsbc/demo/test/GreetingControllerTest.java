aa156125fd16af34cee335a259e12958f9bfa5a4
时光不负 <1097628703@qq.com>
2018-07-31 09:58:14 +0800

    Restful单元测试

--- src/test/java/com/hsbc/demo/test/GreetingControllerTest.java
+++ src/test/java/com/hsbc/demo/test/GreetingControllerTest.java
@@ -2,6 +2,10 @@ package com.hsbc.demo.test;
 
 import com.hsbc.demo.entity.Greeting;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @ClassName GreetingControllerTest
@@ -10,6 +14,9 @@ import org.junit.Test;
  * @Version 1.0
  * @Date:2018/7/30 11:21
  **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class GreetingControllerTest {
 
     @Test
@@ -17,5 +24,30 @@ public class GreetingControllerTest {
         Greeting greeting = new Greeting(1,"Mike");
         System.out.println("打印的内容是：" +greeting);
     }
+
+    @Test
+    public void getRequest() {
+        Greeting greeting = new Greeting(2,"Jack");
+        System.out.println("打印的内容是：" +greeting);
+    }
+
+    @Test
+    public void putRequest() {
+        Greeting greeting = new Greeting(3,"Jack");
+        System.out.println("打印的内容是：" +greeting);
+    }
+
+    @Test
+    public void deleteRequest() {
+        Greeting greeting = new Greeting(2,"Jack");
+        System.out.println("打印的内容是：" +greeting);
+    }
+
+    @Test
+    public void postRequest() {
+        Greeting greeting = new Greeting(4,"Lucy");
+        System.out.println("打印的内容是：" +greeting);
+    }
+    
 
 }
